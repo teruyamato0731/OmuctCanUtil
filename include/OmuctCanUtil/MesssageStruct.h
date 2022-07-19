f9d1fca399e4aa163efde67b56177837afa38f78
11 <77840326+teruyamato0731@users.noreply.github.com>
2022-07-19 15:52:26 +0900

    delete struct

--- include/OmuctCanUtil/MesssageStruct.h
+++ include/OmuctCanUtil/MesssageStruct.h
@@ -3,23 +3,23 @@
 namespace omuct_can_util {
 namespace message_ns {
 
-struct __attribute__((packed)) WhoAmI {
-  Command command;
-};
+// struct __attribute__((packed)) WhoAmI {
+//   Command command;
+// };
 
-struct __attribute__((packed)) HardReset {
-  Command command;
-};
+// struct __attribute__((packed)) HardReset {
+//   Command command;
+// };
 
-struct __attribute__((packed)) SetState {
-  Command command;
-  State state;
-};
+// struct __attribute__((packed)) SetState {
+//   Command command;
+//   State state;
+// };
 
-struct __attribute__((packed)) SetState {
-  Command command;
-  State state;
-};
+// struct __attribute__((packed)) SetState {
+//   Command command;
+//   State state;
+// };
 
 }  // namespace message_ns
 }  // namespace omuct_can_util
